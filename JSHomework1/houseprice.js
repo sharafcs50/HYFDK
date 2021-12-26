704fae41f6342643d161528a9ca35b1f0012bd00
Sharaf <airsol.enac@gmail.com>
2021-12-26 20:19:54 +0500

    changes

--- JSHomework1/houseprice.js
+++ JSHomework1/houseprice.js
@@ -1,10 +1,10 @@
 function volumeInMeters (wide, deep,high) {
-    return (wide * deep * high)
-};
+    return (wide * deep * high);
+}
 
 function gardenSizeInM2 (number){
-    return(number * 1)
-};
+    return(number * 1);
+}
 
 var housePricePeter = volumeInMeters(8,10,10) * 2.5 * 1000 + gardenSizeInM2(100) * 300;
 var housePriceJulia = volumeInMeters(5,11,8) * 2.5 * 1000 + gardenSizeInM2(70) * 300;
