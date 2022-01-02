10d187bd33eb4d9524081a180e14a166cb49d100
Sharaf <airsol.enac@gmail.com>
2022-01-02 20:23:23 +0500

    added for loop exercise

---
+++ JSW2/forLoop.js
@@ -0,0 +1,16 @@
+// Exercise 1
+for (let i=74; i<99; i++) {
+    console.log(i);
+}
+
+// Exercise 2
+
+// String to Log & No. of Times to log
+
+function stingify(stringToLog,numberOfTimesToLog) {
+
+    for (let i=0; i<numberOfTimesToLog; i++){
+    console.log(stringToLog);
+    }
+}
+stingify("sharaf is typing",13);
