b12031516c517230fe12996f00bf6f331c52728f
Sharaf <airsol.enac@gmail.com>
2022-01-12 20:36:59 +0500

    added hw

--- JSW2/student.js
+++ JSW2/student.js
@@ -1,14 +1,21 @@
 const class07Students = ["sharaf", "furqan","cloe", "hamza", "anonymous"];
+
 function addStudentToClass(studentName) {
   // You write code here
+  if ((class07Students.length) <= 6) {
   class07Students.push(studentName);
+  } else {
+    console.log("Cannot add more students");
+  }
 }
 
 function getNumberOfStudents() {
   // You write code here
-  let a = class07Students.length;
+  let a = (class07Students.length);
   console.log(a);
 }
 addStudentToClass("molvi");
 getNumberOfStudents();
 console.log(class07Students);
+console.log(class07Students.length);
+addStudentToClass("mshruuer");
